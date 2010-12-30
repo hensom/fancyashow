62611d75ca2ad581c7972e966d8372bb1152070a
Michael Henson <hensom@gmail.com>
2010-12-29 22:06:36 -0500

    Migrated the Littlefield parser to the new site layout

--- fancyashow/parsers/nyc/littlefield.py
+++ fancyashow/parsers/nyc/littlefield.py
@@ -9,8 +9,8 @@ extensions = ExtensionLibrary()
 
 class Littlefield(ShowParser):
   BASE_URL     = "http://www.littlefieldnyc.com/"
-  CALENDAR_URL = "http://www.littlefieldnyc.com/upcoming-events/"
-  IS_EVENT     = re.compile("http://www.littlefieldnyc.com/event-detail/\?id=\d+")
+  CALENDAR_URL = "http://www.littlefieldnyc.com/calendar"
+  IS_EVENT     = re.compile("http://www.littlefieldnyc.com/event/\d+")
 
   def __init__(self, *args, **kwargs):
     super(Littlefield, self).__init__(*args, **kwargs)
@@ -27,48 +27,45 @@ class Littlefield(ShowParser):
     raise StopIteration
 
   def _get_parser(self):
-    show_urls = html_util.get_show_urls(self.CALENDAR_URL, "ul#events-list", {'tag': 'li'}, self.IS_EVENT, parse_500 = True)
+    show_urls = html_util.get_show_urls(self.CALENDAR_URL, ".tfly-calendar", '.one-event', self.IS_EVENT, parse_500 = True)
     
     for url in show_urls:
-      try:
-        yield self._parse_show(url)
-      except Exception, e:
-        raise ParserError(url, None, e)
+      yield self._parse_show(url)
 
   def _parse_show(self, link):
     event_doc = html_util.fetch_and_parse(link, parse_500 = True)
 
-    event_detail = html_util.get_first_element(event_doc,    ".event-details")
-    event_vitals = html_util.get_first_element(event_detail, ".event-vitals")
-    artist_info  = html_util.get_first_element(event_doc,    ".artist-section")
+    event_detail = html_util.get_first_element(event_doc,    ".event-detail")
+    artist_info  = html_util.get_first_element(event_doc,    ".artist-boxes")
     
-    date_txt     = html_util.get_first_element(event_vitals, ".date").text_content()
+    date_txt     = html_util.get_first_element(event_detail, ".dates").text_content()
     
     performers = [] 
     
-    for tag in ('h2', 'h3'):
-      performers.extend([self._parse_performer(h) for h in event_vitals.iter(tag = tag)])
+    for el in html_util.get_elements(event_doc, '.headliners'):
+      for name in lang_util.parse_performers(el.text_content()):
+        performers.append(Performer(name, headliner = True))
+
+    for el in html_util.get_elements(event_doc, '.supports'):
+      for name in lang_util.parse_performers(el.text_content()):
+        performers.append(Performer(name, headliner = False))
 
     show = Show()
 
     show.merge_key  = link
     show.venue      = self.venue()
     show.performers = performers
-    show.show_time  = date_util.parse_show_time(date_txt, html_util.get_first_element(event_vitals,  ".time").text_content())
-    show.door_time  = date_util.parse_door_time(date_txt, html_util.get_first_element(event_vitals,  ".time").text_content())
+    show.show_time  = date_util.parse_date_and_time(date_txt, html_util.get_first_element(event_detail, ".times").text_content())
 
     show.resources.show_url      = link
-    show.resources.resource_uris = self.resource_extractor.extract_resources(event_vitals, event_detail, artist_info)
+    show.resources.resource_uris = self.resource_extractor.extract_resources(event_detail, artist_info)
 
-    img = html_util.get_first_element(event_detail, ".event-image img", optional = True)
+    img = html_util.get_first_element(event_detail, "img", optional = True)
     
     if img is not None:
       show.resources.image_url = img.get('src')
 
     return show
-    
-  def _parse_performer(self, h):
-    return Performer(h.text_content(), headliner = h.tag == 'h2')
 
   def venue(self):
     return Venue('Littlefield', self.BASE_URL)
