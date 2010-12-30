7a8b75202e1c76bbb6dac74299ec72a57170f325
Michael Henson <hensom@gmail.com>
2010-12-29 21:36:38 -0500

    Europa now detects when the year is likely to have changed and will adjust the year of the shows accordingly

--- fancyashow/parsers/nyc/europa.py
+++ fancyashow/parsers/nyc/europa.py
@@ -2,6 +2,7 @@ import logging
 import urllib2
 import lxml.html
 import re
+from datetime                     import datetime
 from fancyashow.extensions        import ExtensionLibrary, ShowParser
 from fancyashow.extensions.models import Venue, Performer, Show
 from fancyashow.util              import parsing  as html_util
@@ -18,7 +19,9 @@ class Europa(ShowParser):
   def __init__(self, *args, **kwargs):
     super(Europa, self).__init__(*args, **kwargs)
     
-    self._parser = None
+    self._parser    = None
+    self.prev_month = None
+    self.year       = datetime.now().year
     
   def next(self):
     if not self._parser:
@@ -57,13 +60,10 @@ class Europa(ShowParser):
     
     for el in html_util.get_elements(main_section, 'table'):
       if el.get('width') == '700' and el.get('height') == '80':
-        try:
-          show =  self._parse_show(el)
-        
-          if show:
-            yield show
-        except Exception, e:
-          raise ParserError(self.CALENDAR_URL, None, e)
+        show =  self._parse_show(el)
+      
+        if show:
+          yield show
 
   def _parse_show(self, el):
     date_el = html_util.get_first_element(el, '.calendardates')
@@ -92,6 +92,15 @@ class Europa(ShowParser):
     show.venue      = self.venue()
     show.performers = performers
     show.show_time  = date_util.parse_date_and_time(date_txt, time_txt)
+    
+    # Adjust for the date shifting over by one year
+    if show.show_time:
+      if self.prev_month > show.show_time.month:
+        self.year += 1
+
+      show.show_time = show.show_time.replace(year = self.year)
+
+      self.prev_month = show.show_time.month
 
     show.resources.resource_uris = self.resource_extractor.extract_resources(el)
     
