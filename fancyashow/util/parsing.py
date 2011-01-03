40765bf7cac1c2630ee0673eef50f2eefc0dadf3
Michael Henson <hensom@gmail.com>
2011-01-02 23:31:07 -0500

    Added a check on the current value on the string

--- fancyashow/util/parsing.py
+++ fancyashow/util/parsing.py
@@ -120,8 +120,9 @@ def get_displayed_text_content(el):
     if el.tag in SKIP_ELS:
       return
       
-    if el.tag in LINE_BREAK_ELS and text.getvalue()[-1] != '\n':
-      text.write('\n')
+    if el.tag in LINE_BREAK_ELS:
+      if text.getvalue() and text.getvalue()[-1] != '\n':
+        text.write('\n')
 
     if el.text:
       if preserve_text:
